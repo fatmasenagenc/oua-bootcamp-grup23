2cdb4f8431671b33d895d6a286962fff2199b447
Emre <53142970+Emrekeyf@users.noreply.github.com>
2024-07-19 22:14:23 +0300

    sahne tasarımı.
    .

---
+++ oua-bootcamp-2024-g23/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Hareket hýzý
+    public float rotationSpeed = 100f; // Dönüþ hýzý
+
+    void Update()
+    {
+        // Hareket iþlemleri
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveUp();
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            MoveDown();
+        }
+
+        // Dönüþ iþlemi
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateLeft();
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotateRight();
+        }
+
+        // Space tuþuna basýldýðýnda ileri hareket iþlemi
+        if (Input.GetKey(KeyCode.Space))
+        {
+            MoveForward();
+        }
+    }
+
+    void MoveUp()
+    {
+        transform.Rotate(Vector3.left, rotationSpeed * Time.deltaTime);
+        
+    }
+
+    void MoveDown()
+    {
+        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        
+    }
+
+    void RotateLeft()
+    {
+        transform.Rotate(Vector3.down, rotationSpeed * Time.deltaTime);
+        
+    }
+
+    void RotateRight()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+       
+    }
+
+    void MoveForward()
+    {
+        // Karakterin ileri doðru hareket etmesi için Vector3.forward kullanýlýr.
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.Self);
+    }
+}
