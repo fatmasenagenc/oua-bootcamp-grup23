de3e8d64609e4f179a04d5d883a04f2517ed374c
Eray Artıışık <erayart76@gmail.com>
2024-07-29 21:32:10 +0300

    Balina konuşuyor

---
+++ Assets/Script/NPCInteraction.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCInteraction : MonoBehaviour
+{
+    public GameObject dialoguePanel; // Panel UI nesnesini referans alýn
+    public Text dialogueText; // Text UI nesnesini referans alýn
+    public string npcDialogue = "Merhaba, ben NPC! Size nasýl yardýmcý olabilirim?";
+    private bool isDialogueOpen = false;
+    private bool isPlayerNearby = false;
+
+    void Start()
+    {
+        dialoguePanel.SetActive(false); // Baþlangýçta paneli kapalý tut
+    }
+
+    void Update()
+    {
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            if (isDialogueOpen)
+            {
+                CloseDialogue();
+            }
+            else
+            {
+                OpenDialogue();
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = false;
+            CloseDialogue();
+        }
+    }
+
+    void OpenDialogue()
+    {
+        dialoguePanel.SetActive(true); // Paneli etkinleþtir
+        dialogueText.text = npcDialogue; // NPC diyaloðunu ayarla
+        isDialogueOpen = true;
+    }
+
+    void CloseDialogue()
+    {
+        dialoguePanel.SetActive(false); // Paneli devre dýþý býrak
+        isDialogueOpen = false;
+    }
+
+    public void AppendText(string newText)
+    {
+        dialogueText.text += "\n" + newText; // Yeni metni mevcut metne ekle
+    }
+}
