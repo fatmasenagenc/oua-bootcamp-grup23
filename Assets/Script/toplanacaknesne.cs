da3091ce36fcd18613b8ac51d90c46d8bd4c202d
Emre <53142970+Emrekeyf@users.noreply.github.com>
2024-07-30 15:00:53 +0300

    çöpler yerleştirildi ve çöp toplama kod eklendi.
    .

---
+++ Assets/Script/toplanacaknesne.cs
@@ -0,0 +1,48 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class CharacterCollision : MonoBehaviour
+{
+    public int scorePerCollision = 10;
+    public Text countText;
+    private int objectCount;
+
+    public int score = 0;
+    void Start()
+    {
+        objectCount = 0; // Baþlangýçta toplanan obje sayýsýný sýfýrla
+        UpdateCountText(); // Baþlangýçta metin kutusunu güncelle
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        // Karakter çöple çarpýþtýðýnda bu metot çaðrýlýr.
+        // Collision, çöp objesini temsil eden collider bileþenidir.
+
+        if (collision.gameObject.CompareTag("Trash"))
+        {
+            Destroy(collision.gameObject); // Çöpü yok et
+            IncreaseScore(); // Skoru artýr
+            objectCount++; // Toplanan obje sayýsýný bir artýr
+            UpdateCountText(); // Metin kutusunu güncelle
+        }
+    }
+    void UpdateCountText()
+    {
+        countText.text = "Toplanan Objeler: " + objectCount.ToString(); // Metin kutusunu güncelle
+    }
+
+    void IncreaseScore()
+    {
+        score += scorePerCollision;
+
+        // Skoru artýran kod buraya gelecek
+        // Örneðin bir UIManager üzerinden skoru güncelleyebiliriz
+
+        // UIManager.Instance.IncreaseScore(1); // UIManager scriptinde IncreaseScore fonksiyonu çaðrýlýr
+        // UIManager.Instance.UpdateScoreText(); // Skor textini günceller
+    }
+}
+
